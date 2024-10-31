574054999344dca314774ef3e93f8989b4c2fed9
James Leslie <jamesleslie@posteo.net>
2024-10-31 22:10:57 +0000

    Update event queue

--- software/src/app/EventLoop.hs
+++ software/src/app/EventLoop.hs
@@ -1,11 +1,22 @@
-{-# LANGUAGE MonoLocalBinds #-}
-
 module EventLoop (EventLoop (..), addMsg, killEventLoop, mkEventLoop) where
 
-import Control.Concurrent (killThread, newChan, readChan, writeChan)
+import Control.Concurrent (
+    killThread,
+    modifyMVar_,
+    newEmptyMVar,
+    newMVar,
+    putMVar,
+    readMVar,
+    takeMVar,
+    tryPutMVar,
+    tryTakeMVar,
+ )
 import Control.Concurrent.Lifted (fork)
+import Control.Monad (void)
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Control (MonadBaseControl)
+import Data.PQueue.Prio.Min qualified as PQueue
+import Data.Time.Clock.System (SystemTime, getSystemTime)
 
 {- | A single threaded event loop that can have messages added to it
 from another thread.
@@ -30,15 +41,50 @@ mkEventLoop ::
     (msg -> m ())
     -> m (EventLoop msg)
 mkEventLoop handle = do
-    chan <- liftIO $ newChan @msg
-    threadId <- fork $ actOnChan chan
+    -- Block checking the state of the queue. To be used when the queue
+    -- is empty.
+    pauseLoopMVar <- liftIO $ newEmptyMVar @()
+
+    -- MVar around the piority queue
+    pqueueMVar <-
+        liftIO . newMVar $ PQueue.empty @SystemTime @msg
+
+    threadId <- fork $ actOnQueue pauseLoopMVar pqueueMVar
+
+    let addMsg' msg = do
+            time <- getSystemTime
+            -- Insert the messaage into the priority queue with
+            -- current time.
+            modifyMVar_ pqueueMVar $ \queue -> pure $ (time, msg) PQueue.:< queue
+            -- Unblock the loop if it was waiting
+            void $ tryPutMVar pauseLoopMVar ()
+
+    let killEventLoop' = do
+            liftIO . void $ tryPutMVar pauseLoopMVar ()
+            killThread threadId
+
     return $
         EventLoop
-            { _addMsg = writeChan chan
-            , _killEventLoop = killThread threadId
+            { _addMsg = addMsg'
+            , _killEventLoop = killEventLoop'
             }
   where
-    actOnChan chan = do
-        msg <- liftIO $ readChan chan
-        handle msg
-        actOnChan chan
+    actOnQueue pauseLoopMVar mvar = do
+        -- This blocks until pauseLoopMVar is filled, which happens
+        -- when we add an event.
+        _ <- liftIO $ readMVar pauseLoopMVar
+        queue <- liftIO $ takeMVar mvar
+        case queue of
+            PQueue.Empty -> do
+                liftIO . void $ tryTakeMVar pauseLoopMVar
+                liftIO $ putMVar mvar PQueue.Empty
+            (PQueue.:<) (msgTime, msg) remQueue -> do
+                curTime <- liftIO getSystemTime
+                if msgTime <= curTime
+                    then do
+                        -- Make sure to put the remaining queue back to
+                        -- not block other messages being added by other threads.
+                        liftIO $ putMVar mvar remQueue
+                        handle msg
+                    else liftIO $ putMVar mvar queue
+        actOnQueue pauseLoopMVar mvar
