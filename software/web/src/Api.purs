e506e224fb486f5089d5a29959bc206a4534e290
James Leslie <jamesleslie@posteo.net>
2024-12-03 06:32:31 +0000

    Add script to patch purs generated code by purs-proto

--- software/web/src/Api.purs
+++ software/web/src/Api.purs
@@ -2,12 +2,16 @@ module Api where
 
 import Prelude
 
+import Data.Tuple.Nested ((/\))
+import Deku.Effect as DE
+import Effect (Effect)
 import FRP.Poll (Poll)
+import Pages (initialSystemPageState)
 import Pages.System (SystemPageState)
 
 data Api = Api { systemPagePoll :: Poll SystemPageState }
 
 mkApi :: Effect Api
 mkApi = do
-  _ /\ setSystemPageState /\ systemPageState <- DE.useHot initialSystemPageState
-  pure $ Api { systemPagePoll}
+  _ /\ setSystemPagePoll /\ systemPagePoll <- DE.useHot initialSystemPageState
+  pure $ Api { systemPagePoll }
