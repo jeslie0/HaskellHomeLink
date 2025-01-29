d26cf05840f2ec00b19d9db1096c0e0125474ff8
James Leslie <jamesleslie@posteo.net>
2025-01-29 06:13:28 +0000

    Fix error in hls-wrapper

--- nix/hls-wrapper.nix
+++ nix/hls-wrapper.nix
@@ -1,5 +1,5 @@
-{ pkgs }:
+{ pkgs, ghcVersion }:
 pkgs.writeScriptBin "haskell-language-server-wrapper" ''
       #!${pkgs.stdenv.shell}
-      ${pkgs.haskell-nix.tool "ghc927" "haskell-language-server" "latest"}/bin/haskell-language-server "$@"
+      ${pkgs.haskell-nix.tool ghcVersion "haskell-language-server" "latest"}/bin/haskell-language-server "$@"
     ''
